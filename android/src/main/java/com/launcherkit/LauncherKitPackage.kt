ec54566c71a018ae00bfd4a4ab097f879a95cf50
louaySleman <louayakram12@hotmail.com>
2026-04-26 14:33:00 +0300

    [Refactor] Select TurboModule or legacy implementation in LauncherKitPackage.

--- android/src/main/java/com/launcherkit/LauncherKitPackage.kt
+++ android/src/main/java/com/launcherkit/LauncherKitPackage.kt
@@ -5,13 +5,21 @@ import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
 
+/**
+ * React Native package registration for LauncherKit.
+ *
+ * Selects the appropriate module implementation based on the current architecture:
+ * [LauncherKitModuleNew] for TurboModules or [LauncherKitModuleLegacy] for the old bridge.
+ */
 class LauncherKitPackage : ReactPackage {
   override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-    return if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      listOf(LauncherKitModuleNew(reactContext))
-    } else {
-      listOf(LauncherKitModuleLegacy(reactContext))
-    }
+    return listOf(
+      if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+        LauncherKitModuleNew(reactContext)
+      } else {
+        LauncherKitModuleLegacy(reactContext)
+      }
+    )
   }
 
   override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
